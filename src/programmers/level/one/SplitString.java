4b2885a6a37b1d6e1633048093a7074ee7bcf632
Il Young Hwang <ilyoung6016@gmail.com>
2023-06-20 22:56:04 +0900

    Update SplitString.java

--- src/programmers/level/one/SplitString.java
+++ src/programmers/level/one/SplitString.java
@@ -4,20 +4,19 @@ public class SplitString {
     public static void main(String[] args) {
         String answer = "";
         
-	      // 각 월의 마지막값
+	// 각 월의 마지막값
         int[] daysInMonth = {31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
-        
 
-	      // 입력받은 월까지의 일수 구하기
+	// 입력받은 월까지의 일수 구하기
         int totalDays = 0;
         for (int i = 0; i < a-1; i++) {
             totalDays += daysInMonth[i];
         }
         totalDays += b;
 
-	      // 시작 요일 설정
+	// 시작 요일 설정
         int startingDay = 5; 
-	      // 시작요일 index와 전체일수를더하여 7로 나눈 값구하기 
+	// 시작요일 index와 전체일수를더하여 7로 나눈 값구하기 
         int dayOfWeek = (startingDay + totalDays - 1) % 7;
 
         String[] weekdays = {"SUN", "MON", "TUE", "WED", "THU", "FRI", "SAT"};
