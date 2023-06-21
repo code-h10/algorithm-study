443c44759e2e7a040598ae40640348f310ed42e6
Il Young Hwang <ilyoung6016@gmail.com>
2023-06-21 21:27:25 +0900

    Update 키패드 누르기

--- src/programmers/level/one/PressTheKeyPad.java
+++ src/programmers/level/one/PressTheKeyPad.java
@@ -3,6 +3,8 @@ import java.util.*;
 
 public class PressTheKeyPad {
     public static void main(String[] args) {
+        int[] numbers = {1, 3, 4, 5, 8, 2, 1, 4, 5, 9, 5};
+        String hand = "right";
         StringBuilder answer = new StringBuilder();
         int leftNumber = -1;
         int rightNumber = -2;
